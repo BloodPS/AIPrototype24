a1598e007d68e42603b16347b3ff2778e767f2c8
paramesblood <naxmian14@gmail.com>
2025-01-07 18:57:14 +0700

    Homework 1

--- myfristpy.py
+++ myfristpy.py
@@ -5,10 +5,10 @@ def parse_input():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--bd',
-        type=int,
+        '--dob',
+        type=str,
         required=True,
-        help='brirth day user'
+        help='Date of birth in the format DD-MM-YYYY (e.g., 25-12-1990)'
     )
 
     parser.add_argument(
@@ -31,13 +31,18 @@ def parse_input():
 def printHello(who):
     print(f"Hello World!,{who}!!")
 
-def cal_todayVbd(bd):
-    td = datetime.today().strftime('%d')
-    return bd-int(td)
+def calculate_days_from_birth(dob):
+    try:
+        birth_date = datetime.strptime(dob, '%d-%m-%Y')
+        today = datetime.today()
+        delta = today - birth_date
+        return delta.days
+    except ValueError:
+        raise ValueError("Invalid date format. Please use DD-MM-YYYY.")
 
 if __name__ == "__main__":
     input_v = parse_input()
     print('This is my frist .py')
     printHello(input_v.name)
-    print(f'your birthday is {cal_todayVbd (input_v.bd)} from.today')
+    print(f'your were born {calculate_days_from_birth (input_v.dob)} day from birth')
 
